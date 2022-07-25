c14b3831e2e41a11ff8304894416d85cf0f304d6
c1322368 Raiuri Santos de Sousa Pinto <raiuri.pinto@capgemini.com>
2022-07-25 12:26:46 -0300

    chore: add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    watch: false
+  }
 })
