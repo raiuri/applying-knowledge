2070d35d8dbf6fe55dff0c4445dd9a485c64531e
c1322368 Raiuri Santos de Sousa Pinto <raiuri.pinto@capgemini.com>
2022-07-25 18:54:34 -0300

    test: ensure remoteAuthentication calls HttpPostClient with correct body

--- src/data/usercases/authentication/remote-authentication.spec.ts
+++ src/data/usercases/authentication/remote-authentication.spec.ts
@@ -2,6 +2,7 @@ import { describe, test, expect } from 'vitest'
 import { HttpPostClientSpy } from '../../mocks/mock-http-client'
 import { RemoteAuthentication } from './remote-authentication'
 import { faker } from '@faker-js/faker'
+import { mockAuthentication } from '../../../domain/mocks/mock-authentication'
 
 type SutTypes = {
   sut: RemoteAuthentication,
@@ -22,8 +23,16 @@ describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL ', async () => {
     const url = faker.internet.url()
     const { sut, httpPostClientSpy } = makeSut(url)
-    await sut.auth()
+    await sut.auth(mockAuthentication())
 
     expect(httpPostClientSpy.url).toBe(url)
+  })
+
+  test('Should call HttpPostClient with correct body ', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const authenticationParams = mockAuthentication()
+    await sut.auth(authenticationParams)
+
+    expect(httpPostClientSpy.body).toEqual(authenticationParams)
   })
 })
