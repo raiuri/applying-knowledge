abb4412e84fb6272fbd7eb010512a77f012d1e97
c1322368 Raiuri Santos de Sousa Pinto <raiuri.pinto@capgemini.com>
2022-07-25 17:34:29 -0300

    refactor: move SUT creation to a factory method

--- src/data/usercases/authentication/remote-authentication.spec.ts
+++ src/data/usercases/authentication/remote-authentication.spec.ts
@@ -2,14 +2,27 @@ import { describe, test, expect } from 'vitest'
 import { HttpPostClientSpy } from '../../test/mock-http-client'
 import { RemoteAuthentication } from './remote-authentication'
 
+type SutTypes = {
+  sut: RemoteAuthentication,
+  httpPostClientSpy: HttpPostClientSpy
+}
+
+const makeSut = (url: string = 'any url'): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy()
+  const sut = new RemoteAuthentication(url, httpPostClientSpy)
+
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
+
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL ', async () => {
-
-    const url = 'any url'
-    const httpPostClient = new HttpPostClientSpy()
-    const sut = new RemoteAuthentication(url, httpPostClient)
+    const url = 'other_value'
+    const { sut, httpPostClientSpy } = makeSut(url)
     await sut.auth()
 
-    expect(httpPostClient.url).toBe(url)
+    expect(httpPostClientSpy.url).toBe(url)
   })
 })
