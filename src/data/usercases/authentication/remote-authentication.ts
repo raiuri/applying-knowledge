d7c7b0f09e42fd2b6f4a24801a7841468ad7a220
c1322368 Raiuri Santos de Sousa Pinto <raiuri.pinto@capgemini.com>
2022-07-25 17:04:41 -0300

    feat: ensure RemoteAuthentication calls HttpPostClient with correct URL

---
+++ src/data/usercases/authentication/remote-authentication.ts
@@ -0,0 +1,12 @@
+import { HttpPostClient } from "../../protocols/http/httpPostClient";
+
+export class RemoteAuthentication {
+  constructor(
+    private readonly url: string,
+    private readonly httpClient: HttpPostClient
+    ) {}
+
+  async auth(): Promise<void> {
+    this.httpClient.post(this.url)
+  }
+}
